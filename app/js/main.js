28385b646ff5d65cf327c03130f8759beae75046
Max <vosroix@gmail.com>
2021-09-19 11:56:42 +0500

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,36 +1,43 @@
 const slider = document.getElementById('slider');
 const sliderBox = document.getElementById('slider-box');
+let maxShift = slider.offsetWidth - sliderBox.offsetWidth;
+
+const prevSlideBtn = document.getElementById('prevSlide');
+const nextSlideBtn = document.getElementById('nextSlide');
+
 
 class Slider {
-	constructor(str) {
-		this.str = str;
+	constructor() {
 		this.shifted = 0;
+		this.slowing = 0.9;
 	}
 
 	shiftX(x) {
 		sliderBox.style.transform = `translateX(${x}px)`;
 	}
 
+	slideToNext() {
+		let activeSlide = sliderBox.querySelector('.active');
+		console.log(activeSlide)
+	}
+
 
 	init() {
 
-		slider.ondragstart = () => false;                       //убираем по-умолчанию
+		slider.ondragstart = () => false;      //убираем по-умолчанию
 
 		slider.onmousedown = (event) => {   //при нажатии 
 			let beginX = event.clientX;
 
-
 			slider.onmousemove = (event) => { //при перемещении
 				
-				let shiftX = beginX - event.clientX;
-				shiftX = -shiftX * 0.8;
+				let shiftX = (event.clientX - beginX) * this.slowing;
+				shiftX += this.shifted;
 
-				shiftX = shiftX + this.shifted;
-				
-				this.shiftX(shiftX);
-	
+				if (shiftX <= 0 && shiftX > maxShift) {
+					this.shiftX(shiftX);
+				}
 			}
-
 		}
 
 		document.onmouseup = () => {  // при отпускании ЛКМ - отмена 
@@ -38,11 +45,16 @@ class Slider {
 			this.shifted = sliderBox.style.transform.match(/-?\d+(.\d+)?/gm)[0]; //сколько сдвинул?
 			this.shifted = parseInt(this.shifted);
 		}
+
+		nextSlideBtn.onclick = e => {
+			this.slideToNext();
+			console.log('s')
+		}
 	}
 }
 
 
 
-const slider1 = new Slider();
+const slider = new Slider();
 
-slider1.init();
+slider.init();
